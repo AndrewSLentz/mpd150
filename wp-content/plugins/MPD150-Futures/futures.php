8066fb454757ee49010531c49e86ca3a77674200
petervkay <petervkay@gmail.com>
2017-10-19 16:35:25 -0500

    added futures and interviews post types

---
+++ wp-content/plugins/MPD150-Futures/futures.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Plugin Name: MPD150 Futures
+Description: Used to generate the futures grid
+Version: 1.0
+Author: Peter VanKoughnett
+Author URI: bigriverwebdesign.com
+License: GPLv2 or later
+*/
+
+function create_posttype_future() {
+ 
+    register_post_type( 'futures',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Futures' ),
+                'singular_name' => __( 'Future' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'Futures'), 
+            'supports' => array('editor','title','author')
+        )
+    );
+}
+add_action( 'init', 'create_posttype_future' );
---
+++ wp-content/plugins/MPD150-Interviews/interviews.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Plugin Name: MPD150 Interviews
+Description: Used to generate the interviews grid
+Version: 1.0
+Author: Peter VanKoughnett
+Author URI: bigriverwebdesign.com
+License: GPLv2 or later
+*/
+
+function create_posttype_interview() {
+ 
+    register_post_type( 'interviews',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Interviews' ),
+                'singular_name' => __( 'Interview' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'Interviews'), 
+            'supports' => array('title','author')
+        )
+    );
+}
+add_action( 'init', 'create_posttype_interview' );
