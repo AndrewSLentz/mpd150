edad1980a20d6ba8a456ff1b344d4f656b92c031
petervkay <petervkay@gmail.com>
2017-11-08 16:27:23 -0600

    plugins

--- wp-content/plugins/MPD150-Futures/futures.js
+++ wp-content/plugins/MPD150-Futures/futures.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($){
 	$('.modal').on('shown.bs.modal', function (e) {
+		var modal = $(this);
 		var topicID = $(this).attr("id");
 		var list = $(".acf-field-taxonomy .acf-checkbox-list", this);
 		list.children("li").each(function(){
@@ -9,9 +10,15 @@ jQuery(document).ready(function($){
 			}
 		});
 
-		$(".acf-field-textarea", this).change(function(){
+		$(".acf-field-textarea", this).keyup(function(){
+			var postTitle = modal.find(".acf-field--post-title input");
+			var contentInput = $("textarea",this).val();
+			console.log(contentInput);
 			console.log('change');
+			postTitle.val(contentInput);
 		})
+
+		
 
 
 	});
