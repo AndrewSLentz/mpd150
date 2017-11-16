db7823558f4da6a0655f4a87972d3df6decd5eb7
petervkay <petervkay@gmail.com>
2017-11-16 11:00:04 -0600

    fixing pull quotes + citations

--- wp-content/themes/mpd150/functions.php
+++ wp-content/themes/mpd150/functions.php
@@ -89,7 +89,7 @@ if ( current_user_can( 'manage_options' ) ) {
 define('ACF_EARLY_ACCESS', '5'); //allows upgrading to ACF 5
 
 function pull_quote($atts=[],$content=null) {
-	$content = "<span class='pull-quote'>" . $content . "</span>";
+	$content = do_shortcode("<span class='pull-quote'>" . $content . "</span>");
 	return $content;
 }
 
