772bb123765029b0dd4f4f35e08e297fd35b98c5
petervkay <petervkay@gmail.com>
2017-11-16 14:50:31 -0600

    basic present page

--- wp-content/themes/mpd150/page-present.php
+++ wp-content/themes/mpd150/page-present.php
@@ -17,7 +17,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<div class='row justify-content-center color4'>
 						<div class='col-12 col-md-10 col-lg-10'>
 							<h1> <?php the_title() ?></h1>
-							<div class='aligncenter text-center margin-lg trans-white'><?php the_field('present_content') ?></div>		
+															<div class='aligncenter text-left margin-lg trans-white'><?php the_field('present_summary') ?></div>
+								<div class='aligncenter text-left margin-lg trans-white content hidden'><?php the_field('present_full_content')?></div>
+								<div class= 'report-button-container'>
+									<button class='read-more'>Read More</button>
+									<button class='view-feature'>View Timeline</button>
+								</div>
+	
 						</div>
 					</div>
 
